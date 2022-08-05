fb63b65dda8c7ab4286bd1d5fd4ea9800a25ae60
jtaru28912 <61678840+jtaru28912@users.noreply.github.com>
2022-08-05 18:35:01 +0530

    Add files via upload

---
+++ springbootproject/src/main/java/org/onlyforjavatech/Test.java
@@ -0,0 +1,16 @@
+package org.onlyforjavatech;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class Test {
+	@RequestMapping("/test")
+	public String firstHandler() {
+		return "index";		
+	}
+	
+}
+//request and response ->request to the server to the response
